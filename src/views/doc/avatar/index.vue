fd96ccd6abc99bf0fab9b347a11c1e46146e5848
Equal_xp <2106705593@qq.com>
2023-08-04 16:40:04 +0800

    avatar组件的解释表

--- src/views/doc/avatar/index.vue
+++ src/views/doc/avatar/index.vue
@@ -10,10 +10,8 @@
       <Doc title="属性" type="prop" :body="propsDoc" />
       <Doc title="事件" type="event" :body="eventDoc" />
       <Doc title="插槽" type="slot" :body="slotDoc" />
-      <PrevAndNext
-        :prev="{ path: '/doc/get-started', name: '快速使用' }"
-        :next="{ path: '/doc/button', name: '按钮 Button' }"
-      />
+      <PrevAndNext :prev="{ path: '/doc/get-started', name: '快速使用' }"
+        :next="{ path: '/doc/button', name: '按钮 Button' }" />
     </div>
   </div>
 </template>
@@ -29,20 +27,29 @@ import AvatarPreview4 from "./AvatarPreview4.preview.vue";
 
 
 const propsDoc = [
+  // 属性 说明 类型 可选值 默认值
   [
-    
-  ]
+    "size",
+    "设置头像大小",
+    "number / string",
+    "number / large / default / small",
+    "default",
+  ],
+  ["shape", "设置头像的形状", "string", "circle / square", "circle"],
+  ["src", "图片头像的资源地址", "string", "-", "-"],
+  ["alt", "描述图像的替换文本", "string", "-", "-"],
 ]
 const eventDoc = [
   [
-
-  ]
+    "error",
+    "图片类头像加载失败的回调， 返回 false 会关闭组件默认的 fallback 行为",
+    "(e: Event)",
+  ],
 ]
 const slotDoc = [
-  [
+  ["default", "自定义头像展示内容"]
+];
 
-  ]
-]
 </script>
 
 <style lang="scss">
