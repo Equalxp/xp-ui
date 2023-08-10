354a20d8996dca039dd5d25bc4d4dd07a9988a4c
Equal_xp <2106705593@qq.com>
2023-08-11 00:34:27 +0800

    select+文档

--- src/views/doc/select/index.vue
+++ src/views/doc/select/index.vue
@@ -6,8 +6,12 @@
       <Preview :component="SelectPreview2 " />
       <Preview :component="SelectPreview3 " />
       <Preview :component="SelectPreview4 " />
-
-
+      <Doc title="属性" type="prop" :body="propDoc"></Doc>
+      <Doc title="事件" type="event" :body="eventDoc"></Doc>
+      <PrevAndNext
+        :prev="{ path: '/doc/input', name: 'Input 输入框' }"
+        :next="{ path: '/doc/switch', name: '开关 Switch' }"
+      />
 
     </div>
   </div> 
@@ -22,6 +26,17 @@ import SelectPreview2 from "./SelectPreview2.preview.vue";
 import SelectPreview3 from "./SelectPreview3.preview.vue";
 import SelectPreview4 from "./SelectPreview4.preview.vue";
 
+const propDoc = [
+  ["v-model", "绑定值", "	boolean / string / number", "-", "-"],
+  ["placeholder", "占位符", "string", "-", "请选择"],
+  ["clearable", "是否可以清空选项", "boolean", "-", "false"],
+  ["disabled", "是否禁用", "boolean", "-", "false"],
+  ["options", "配置选项内容", "Array", "-", "[]"],
+]
+const eventDoc = [
+  ["change", "选中值发生变化时触发", "目前的选中值"],
+  ["clear", "在点击由 clearable 属性生成的清空按钮时触发", "-"],
+];
 
 </script>
 
