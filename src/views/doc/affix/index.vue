517ac5afd35553c755c5bad5e0e3445d580c3e83
Equal_xp <2106705593@qq.com>
2023-08-10 15:49:04 +0800

    affix组件-target

--- src/views/doc/affix/index.vue
+++ src/views/doc/affix/index.vue
@@ -2,9 +2,9 @@
   <div class="affix-doc-wrapper">
     <h1>固钉 Affix</h1>
     <p>通过滚动时对目标元素进行判断是否即将离开可视区域，当即将离开可视区域时将目标元素固定在特定可视区域</p>
-    <div class="preview-wrapper" style="height: 1000px;">
+    <div class="preview-wrapper" style="height: 3000px; padding: 1000px 0">
       <Preview :component="AffixPreview1" />
-      <!-- <Preview :component="AffixPreview2" /> -->
+      <Preview :component="AffixPreview2" />
 
     </div>
   </div>
