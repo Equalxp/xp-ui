26531254f0478b9fd1650c3562559e37116a30c0
Equal_xp <2106705593@qq.com>
2023-08-10 15:50:09 +0800

    affix组件-顶部固钉

--- src/views/doc/affix/AffixPreview1.preview.vue
+++ src/views/doc/affix/AffixPreview1.preview.vue
@@ -3,7 +3,4 @@
   <xp-affix :offset="120" position="top">
     <xp-button>距离顶部120px固定</xp-button>
   </xp-affix>
-  <xp-affix :offset="120" position="bottom">
-    <xp-button>距离底部120px固定</xp-button>
-  </xp-affix>
 </template>
